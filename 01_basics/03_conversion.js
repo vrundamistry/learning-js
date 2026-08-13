dda16b1bdddd7fffc035b725e2ee1d542a82e8b4
vrundamistry <106440059+vrundamistry@users.noreply.github.com>
2026-08-13 10:08:50 +0000

    conversion of datat type

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,39 @@
+let score = "gfdgfd"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+// "abc" => NaN
+
+
+
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+// null => faalse
+// undefined => false
+
+
+
+let age = 45
+
+console.log(typeof age)
+
+let stringAge = String(age);
+console.log(typeof stringAge)
+console.log(stringAge)
