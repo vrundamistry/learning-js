173fc612e482534613b043dd0a64227bf3bc8b11
vrundamistry <106440059+vrundamistry@users.noreply.github.com>
2026-08-13 10:49:42 +0000

    operations of data

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,55 @@
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*4)
+// console.log(2**4)
+// console.log(2/4)
+// console.log(2%3)
+
+
+let str1 = "vrunda"
+let str2 = " mistry"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + "2" + 3)
+// console.log("1" + 2 + 2)
+// console.log(1 + "2" + "2")
+// console.log(1 + 2 + "2")
+
+// console.log( (3 + 4) * 5 % 3)
+
+
+// console.log(true) // true
+// console.log(+true) // 1
+// // console.log(true+) // error
+// console.log(+"") // 0
+
+
+let counter = 50
+++counter; // 51
+counter++; // 51
+// console.log(counter)
+
+
+let x = 3;
+let y = x++;
+
+console.table([x, y])  // x = 4; y = 3
+
+
+let a = 5;
+let b = ++a;
+
+console.table([a, b]) // a  = 6; b = 6
+
+
+
+
