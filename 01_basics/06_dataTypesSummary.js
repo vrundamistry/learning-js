a5b4291d291d5918620a825e9e161bf7429fc356
vrundamistry <106440059+vrundamistry@users.noreply.github.com>
2026-08-14 17:26:06 +0000

    Refactor data type summary to include console logs for type checking and memory demonstration

--- 01_basics/06_dataTypesSummary.js
+++ 01_basics/06_dataTypesSummary.js
@@ -50,7 +50,44 @@ const myFunction = function () {
 }
 
 
+// console.log(typeof fruits);
+// console.log(typeof myObj);
+// console.log(typeof myFunction);
 
-console.log(typeof fruits);
-console.log(typeof myObj);
-console.log(typeof myFunction);
+
+
+
+
+// ############### Memory ################
+
+// Stack Memory (Primitive) [Call by Value]
+
+let myName = "Vrunda"
+
+let anotherName = myName
+anotherName = "Rinku"
+
+console.log(myName)
+console.log(anotherName)
+
+
+
+
+// Heap Memory (Non-Primitive) [Csll by Reference]
+
+let userOne = {
+    email : "user@test.com",
+    age : 30
+}
+
+// let userTwo = {
+//     email : "user2@test.com",
+//     age : 45
+// }
+
+
+let userTwo = userOne;
+userTwo.email = "rinku@test.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
