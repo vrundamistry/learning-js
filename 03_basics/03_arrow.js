0b0c78d8c417334485ceb94a5885565289399575
vrundamistry <106440059+vrundamistry@users.noreply.github.com>
2026-09-09 12:23:17 +0000

    Add user object and arrow function examples with console logs

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,58 @@
+const user = {
+    username: "Vrunda",
+    age: 33,
+    welcomeMessage: function(){
+        console.log(`${this.username}, wolcome to website.`)
+        // console.log(this);
+    }
+}
+
+// user.welcomeMessage();
+// user.username = "Jhon";
+// user.welcomeMessage();
+
+// console.log(this);
+
+
+//--------------------------------------
+
+
+// function test(){
+//     let username = "Jhon";
+//     console.log(this.username)
+// }
+
+// const test = function(){
+//     let username = "Jhon";
+//     console.log(this.username)
+// }
+
+const test = () => {
+    let username = "Jhon";
+    console.log(this.username)
+}
+
+// test()
+
+
+
+//--------------------------------------
+
+// // explicit return
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// // implicit return
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// console.log(addTwo(3, 5));
+
+
+const addTwo = (num1, num2) => ({username: "Jhon"})
+ 
+console.log(addTwo());
+
+
